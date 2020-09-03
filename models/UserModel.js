f314faaa5fee4ff2468f72a543d280ee34511e51
Foxterino <foxterinogg@gmail.com>
2020-09-03 15:11:13 +0300

    Added UserModel

---
+++ models/UserModel.js
@@ -0,0 +1,34 @@
+import { Model } from 'objection';
+
+export class UserModel extends Model {
+  static get tableName() {
+    return 'users';
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
+
+  static createUser(userData) {
+    return this.query().insert(userData);
+  }
+
+  static findUser({ username, password }) {
+    return this.query().findOne({ username, password }).throwIfNotFound();
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['email', 'username', 'password'],
+      properties: {
+        id: { type: 'integer' },
+        email: { type: 'string', minLength: 1 },
+        username: { type: 'string', minLength: 1 },
+        password: { type: 'string', minLength: 1 },
+        created_at: { type: 'string' },
+        updated_at: { type: 'string' },
+      },
+    };
+  }
+}
