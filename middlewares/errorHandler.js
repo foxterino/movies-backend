bd0889883de39e816f210447d878982870384259
Foxterino <foxterinogg@gmail.com>
2020-09-14 11:02:10 +0300

    Improved error shape

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,8 +2,10 @@ export const errorHandler = () => async (ctx, next) => {
   try {
     await next();
   } catch (error) {
-    ctx.status = error.status || 500;
-    ctx.body = error.message;
+    const status = error.status || 500;
+
+    ctx.status = status;
+    ctx.body = { code: status, message: error.message };
 
     ctx.app.emit('error', error, ctx);
   }
