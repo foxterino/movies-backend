1c91d5511e90ae1c6df943f93a52cc4ef4d0c3b8
Foxterino <foxterinogg@gmail.com>
2020-09-14 11:01:43 +0300

    Added cors middleware

--- loaders/koa.js
+++ loaders/koa.js
@@ -1,4 +1,5 @@
 import KoaLogger from 'koa-logger';
+import cors from '@koa/cors';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
 import { createRouter } from '../api';
@@ -7,6 +8,7 @@ import { errorHandler } from '../middlewares/errorHandler';
 export const koaLoader = ({ app }) => {
   app.use(errorHandler());
 
+  app.use(cors({ credentials: true }));
   app.use(json());
   app.use(KoaLogger());
   app.use(bodyParser());
