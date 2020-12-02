c1a1811c73edbfac161f371823c6e0ae96ba47cb
Foxterino <foxterinogg@gmail.com>
2020-12-02 16:29:48 +0200

    Fixed knex config and scripts

--- bin/postgres.insert.js
+++ bin/postgres.insert.js
@@ -1,8 +1,10 @@
 import movies from '../data/movies.json';
+import genres from '../data/genres.json';
 import comments from '../data/comments.json';
 import { app } from '../boot';
 import { Model } from 'objection';
 import { MovieModel } from '../models/MovieModel';
+import { GenreModel } from '../models/GenreModel';
 import { CommentModel } from '../models/CommentModel';
 
 const initDatabase = async () => {
@@ -11,7 +13,12 @@ const initDatabase = async () => {
   try {
     await movies.reduce(async (moviesAcc, movie, index) => {
       const { id } = await MovieModel.createMovie(movie);
+      const movieGenres = genres[index];
       const movieComments = comments[index];
+
+      await movieGenres.reduce(async (genresAcc, genre) => {
+        await GenreModel.createGenre({ movieId: id, ...genre });
+      }, Promise.resolve());
 
       await movieComments.reduce(async (commentsAcc, comment) => {
         await CommentModel.createComment({ movieId: id, ...comment });
